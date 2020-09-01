bfc1ff8b920979a7608b60b3e01d1129f41f7639
Bryan D. Gonzalez <bryan.dujari@gmail.com>
2020-09-01 19:35:30 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,6 +4,7 @@ require 'pry'
 def load_library(file)
   # code goes here
     file.each_with_object({}) do |(key, value), hash|
+      binding.pry
       hash[key] = {english: => value[0], japanese: => value[1]}
     end
 end
