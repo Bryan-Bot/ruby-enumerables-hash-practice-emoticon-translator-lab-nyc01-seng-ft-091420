f1fd3fd0cd65083b47712903b5683d601a245d64
Bryan D. Gonzalez <bryan.dujari@gmail.com>
2020-09-01 19:53:38 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,7 +5,11 @@ require 'pry'
 
 def load_library(path)
   # code goes here
-  
+  far = YAML.load_file(path)
+  far.each_with_object({}) do |(key, value), new_hash|
+    new_hash[key] = {:english => value[0], :japanese => value[1]}
+  end
+  new_hash
 end
 
 def get_japanese_emoticon
