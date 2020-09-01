1f4380e22d6a4912a152a6028e55c7eb30b90fa3
Bryan D. Gonzalez <bryan.dujari@gmail.com>
2020-09-01 19:58:59 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -9,8 +9,8 @@ def load_library(path)
   new_hash = {}
   far.each do |key, value|
     new_hash[key] = {}
-    new_hash[key][:english] = {value[0]}
-    new_hash[key][:japanese] = {value[1]}
+    new_hash[key][:english] = value[0]
+    new_hash[key][:japanese] = value[1]
   end
 end
 
