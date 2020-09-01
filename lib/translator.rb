c1e010a976e23e9cace2ae9738ddbe371653d30d
Bryan D. Gonzalez <bryan.dujari@gmail.com>
2020-09-01 19:45:13 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,9 +5,10 @@ require 'pry'
 
 def load_library(file)
   # code goes here
-  far = YMAL.load_file(file)
-  binding.pry
-
+  far = YMAL.load_file('emoticons.yml')
+  far.each_with_object({}) do |(key, value), new_hash|
+    new_hash[key] = {:english => value[0], :japanese => value[1]}
+  end
 end
 
 def get_japanese_emoticon
